034c5bf1816e2a4908946c216a34305911d143ec
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-05-25 18:42:06 -0400

    connect to local host 3000

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require("express");
 const path = require("path");
 
 // init app
-
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
 
 // set up the public folder
-
 app.use(express.static(path.join(__dirname, "public")));
+
+
+// start server
+app.get('/', function(req, res) {
+    res.send("work");
+});
+
+const port = 3000;
+app.listen(port, function(){
+    console.log("server listening......" + port)
+})
